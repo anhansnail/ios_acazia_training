d01730f26c1562edecd7d334088540941dc9d31c
“minhanh” <vuongminhanhsnail@gmail.com>
2021-11-26 10:13:24 +0700

    commit 1

--- exam_2/main.swift
+++ exam_2/main.swift
@@ -7,8 +7,12 @@
 //
 
 import Foundation
-//import SwiftJSON
 
+/*
+ Exam 1:
+ Block Letters
+ Display your initials on the screen in block letters and create an ASCII art.
+ */
 func displayInitial() -> String {
     print("*******************************************")
     print("**VV*******VV**MMM*****MMM*******AAA*******")
@@ -21,6 +25,11 @@ func displayInitial() -> String {
     return ""
 }
 
+/*
+ Exam 2:
+ Quadratic Formula
+ Write a Swift program that solves the quadratic equation.
+ */
 func quadraticEquation(a: Float, b: Float, c: Float) -> String {
     var x1, x2 :Float
     if a != 0 {
@@ -38,7 +47,58 @@ func quadraticEquation(a: Float, b: Float, c: Float) -> String {
     }
     return ""
 }
+
+func quadraticEquationMain () {
+    print("Welcome to resolve quadratic equation feature:")
+    print("Please text a, b, c following \"ax^2 + bx + c = 0\": ")
+    var a: Float? = 0
+    var b: Float? = 0
+    var c: Float? = 0
+    a = Float(readLine()!) ?? a
+    b = Float(readLine()!) ?? b
+    c = Float(readLine()!) ?? c
+    if a != 0 {
+        quadraticEquation(a: a!, b: b!, c: c!)
+    } else {
+        print("Error quadratic equation")
+    }
+    return
+}
+
 /*
+ Exam 3:
+ Piggy Bank
+ You just returned from a trip to South America and you came back with three different kinds of currencies. Let's convert them to USD!
+ */
+func exchangeUSD(money: Int, currency: String) -> String {
+    let metaData: [String: Float] = ["peso": 0.023, "vnd": 0.000044, "canada": 0.79]
+    let result = {(money: Float, rate: Float) -> Float in
+        return money*rate
+    }
+    print(" \(money) peso we get \(result(Float(money), metaData[currency]!)) usd")
+    return ""
+}
+
+func exchangeUSDMain () {
+    let currencies = ["peso", "vnd", "canada"]
+    print("Welcome to exchange USD feature:")
+    print("Our change service currency 1: peso, 2: vnd, 3: canada")
+    print("Press currency and money that you want to change following money, currency")
+    var money: Int? = 0
+    var currency: String? = ""
+    money = Int(readLine()!) ?? money
+    currency = readLine()
+    while !currencies.contains(currency!) {
+        print("We have no \(currency!) currency, please press other:")
+        currency = readLine()!
+    }
+    exchangeUSD(money: money!, currency: currency!)
+    return
+}
+
+/*
+ Exam 4:
+ Fizz Buzz
  Write a program that prints the numbers from 1 to 100. But for multiples of 3 print "Fizz" instead of the number and for the multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5 print "FizzBuzz".
  */
 
@@ -56,10 +116,19 @@ func fizzBuzz(a: Int) -> String {
     }
     return ""
 }
+func fizzBuzzMain () {
+    print("Welcome to fizzbuzz feature:")
+    //    print("Please press end of range interger number greater than 1:")
+    //    let end = Int(readLine()!)
+    fizzBuzz(a: 100)
+}
 
 /*
+ Exam 5:
+ Palindrome
  Create a Palindrome checker by iterating through an array.
  */
+
 func palindromeChecker(a: [Int]) -> String {
     let length = a.count
     let _ = length/2
@@ -76,21 +145,29 @@ func palindromeChecker(a: [Int]) -> String {
     }
     return ""
 }
-/*
- You just returned from a trip to South America and you came back with three different kinds of currencies. Let's convert them to USD!
- */
-func exchangeUSD(money: Int, currency: String) -> String {
-    //    var result: Double
-    let metaData: [String: Float] = ["peso": 0.023, "vnd": 0.000044, "canada": 0.79]
-    let result = {(money: Float, rate: Float) -> Float in
-        return money*rate
+
+func palindromeCheckerMain () {
+    print("Welcome to palindrome checker feature:")
+    print("Please length of numbers: ")
+    let length = Int(readLine()!)
+    var palinderome: [Int] = []
+    if (length! > 1) {
+        for index in 0..<(length!) {
+            print("Please press number \(index+1)")
+            var a:Int? = 0
+            a = Int(readLine()!) ?? a
+            palinderome.append(a!)
+        }
     }
-    print(" \(money) peso we get \(result(Float(money), metaData[currency]!)) usd")
-    return ""
+    palindromeChecker(a: palinderome)
 }
+
 /*
+ Exam 6:
+ Rock, Paper, Scissors
  In this lesson, you’ve learned about and explored the power of functions. In this project, you’ll be honing your knowledge of functions and previous Swift concepts by building a rock, paper, scissors game!
  */
+
 func checkWinner(a: String, b: String) -> Int {
     let r = "rock"
     let p = "paper"
@@ -112,59 +189,12 @@ func random() -> String {
     }
     return ""
 }
-func quadraticEquationMain () {
-    print("Welcome to resolve quadratic equation feature:")
-    print("Please text a, b, c following \"ax^2 + bx + c = 0\": ")
-    var a: Float? = 0
-    var b: Float? = 0
-    var c: Float? = 0
-    a = Float(readLine()!) ?? a
-    b = Float(readLine()!) ?? b
-    c = Float(readLine()!) ?? c
-    if a != 0 {
-        quadraticEquation(a: a!, b: b!, c: c!)
-    } else {
-        print("Error quadratic equation")
-    }
-    return
-}
-func exchangeUSDMain () {
-    let currencies = ["peso", "vnd", "canada"]
-    print("Welcome to exchange USD feature:")
-    print("Our change service currency 1: peso, 2: vnd, 3: canada")
-    print("Press currency and money that you want to change following money, currency")
-    var money: Int? = 0
-    var currency: String? = ""
-    money = Int(readLine()!) ?? money
-    currency = readLine()
-    while !currencies.contains(currency!) {
-        print("We have no \(currency!) currency, please press other:")
-        currency = readLine()!
-    }
-    exchangeUSD(money: money!, currency: currency!)
-    return
-}
-func fizzBuzzMain () {
-    print("Welcome to fizzbuzz feature:")
-    //    print("Please press end of range interger number greater than 1:")
-    //    let end = Int(readLine()!)
-    fizzBuzz(a: 100)
-}
-func palindromeCheckerMain () {
-    print("Welcome to palindrome checker feature:")
-    print("Please length of numbers: ")
-    let length = Int(readLine()!)
-    var palinderome: [Int] = []
-    if (length! > 1) {
-        for index in 0..<(length!) {
-            print("Please press number \(index+1)")
-            var a:Int? = 0
-            a = Int(readLine()!) ?? a
-            palinderome.append(a!)
-        }
-    }
-    palindromeChecker(a: palinderome)
-}
+
+/*
+ Exam 7:
+ Build a Pokédex
+ In this project, you will be using Swift classes to help Professor Oak categorize all the different Pokémons into an electronic device — a Pokédex.
+ */
 enum pokemonTypes {
     case Bug,
     Dark,
@@ -211,7 +241,13 @@ func pokemonMain () {
         pokemonObject(id: 1, name: "Bulbasaur", captured: false, imgUrl: "http://img.pokemondb.net/artwork/bulbasaur.jpg", pokemonTypes: ["Grass", "Poison"]),
         pokemonObject(id: 2, name: "Ivysaur", captured: false, imgUrl: "http://img.pokemondb.net/artwork/ivysaur.jpg", pokemonTypes: ["Grass", "Poison"]),
         pokemonObject(id: 3, name: "Venusaur", captured: false, imgUrl: "http://img.pokemondb.net/artwork/venusaur.jpg", pokemonTypes: ["Grass", "Poison"]),
-        pokemonObject(id: 4, name: "Charmander", captured: false, imgUrl: "http://img.pokemondb.net/artwork/charmander.jpg", pokemonTypes: ["Fire"])
+        pokemonObject(id: 4, name: "Charmander", captured: false, imgUrl: "http://img.pokemondb.net/artwork/charmander.jpg", pokemonTypes: ["Fire"]),
+        pokemonObject(id: 5, name: "Charmeleon", captured: false, imgUrl: "http://img.pokemondb.net/artwork/charmeleon.jpg", pokemonTypes: ["Fire"]),
+        pokemonObject(id: 6, name: "Charizard", captured: false, imgUrl: "http://img.pokemondb.net/artwork/charizard.jpg", pokemonTypes: ["Fire", "Flying"]),
+        pokemonObject(id: 7, name: "Squirtle", captured: false, imgUrl: "http://img.pokemondb.net/artwork/squirtle.jpg", pokemonTypes: ["Water"]),
+        pokemonObject(id: 8, name: "Wartortle", captured: false, imgUrl: "http://img.pokemondb.net/artwork/wartortle.jpg", pokemonTypes: ["Water"]),
+        pokemonObject(id: 9, name: "Blastoise", captured: false, imgUrl: "http://img.pokemondb.net/artwork/blastoise.jpg", pokemonTypes: ["Water"]),
+        pokemonObject(id: 10, name: "Caterpie", captured: false, imgUrl: "http://img.pokemondb.net/artwork/caterpie.jpg", pokemonTypes: ["Bug"])
     ]
     
     let displayPokemonList = {(pokemons: [pokemonObject]) -> String in
@@ -221,8 +257,6 @@ func pokemonMain () {
                 check += 1
                 print("Pokemon infomation number \(check):\n  id: \(pokemon.id), name: \(pokemon.name), captured: \(pokemon.captured), iamge url: \(pokemon.imgUrl), category: \(pokemon.pokemonTypes)")
             }
-        } else {
-            // item could not be found
         }
         return ""
     }
@@ -253,15 +287,33 @@ func pokemonMain () {
         }
         return ""
     }
+    let pokemonListBySearch = {(value: String, pokemons: [pokemonObject]) -> String in
+        var filterResult: [pokemonObject] = []
+        print("Pokemon info belong to \(value) category:")
+        for pokemon in pokemons {
+            let checkContainSubstring = {(poTypes: [String], subType: String) -> Bool in
+                for type in poTypes {
+                    if type.contains(subType) {
+                        return true
+                    }
+                }
+                return false
+            }
+            if checkContainSubstring(pokemon.pokemonTypes, value) || pokemon.name.contains(value) {
+                pokemon.info()
+            }
+        }
+        return ""
+    }
     var feature = 0
     let elements = [1, 2, 3, 4, 5]
     repeat {
         print("1: Display list pokemon")
-        print("2: Get pokemon info by id.")
+        print("2: Get pokemon info by word.")
         print("3: Get pokemon info by name.")
         print("4: Get list pokemon by category")
         print("5: Out:")
-        print("please choose choice your feature: ")
+        print("please choose your feature: ")
         feature = Int(readLine()!) ?? 0
     } while (!elements.contains(feature))
     
@@ -270,9 +322,9 @@ func pokemonMain () {
         case 1:
             displayPokemonList(pokemons)
         case 2:
-            print("Press Pokemon Id that you impress: ")
-            let poId = Int(readLine()!) ?? 0
-            pokemonInfoById(poId, pokemons)
+            print("Press Pokemon word that you impress: ")
+            let key = String(readLine()!)
+            pokemonListBySearch(key, pokemons)
         case 3:
             print("Press Pokemon Name that you impress: ")
             let poName = String(readLine()!)
@@ -304,8 +356,8 @@ func introduction() {
         print("5: Create a Palindrome checker by iterating through an array.")
         print("6: In this lesson, you’ve learned about and explored the power of functions. In this project, you’ll be honing your knowledge of functions and previous Swift concepts by building a rock, paper, scissors game!")
         print("7: In this project, you will be using Swift classes to help Professor Oak categorize all the different Pokémons into an electronic device — a Pokédex.")
-        print("8: Out:")
-        print("please choose choice your feature: ")
+        print("8: Out!")
+        print("please choose your feature: ")
         choice = Int(readLine()!) ?? 0
     } while (!elements.contains(choice))
     
@@ -334,14 +386,17 @@ func introduction() {
 }
 
 
-pokemonMain()
-//introduction()
-//palindromeCheckerMain()
-//var choice = readLine()
-//random()
-//exchangeUSD(money: 123, currency: "vnd")
-//palindromeChecker(a: [1, 2, 2, 1])
-//quadraticEquation(a: 1, b: 5, c: 1)
-//fizzBuzz(a: 100)
+introduction()
+
+/* // test functions
+ pokemonMain()
+ palindromeCheckerMain()
+ var choice = readLine()
+ random()
+ exchangeUSD(money: 123, currency: "vnd")
+ palindromeChecker(a: [1, 2, 2, 1])
+ quadraticEquation(a: 1, b: 5, c: 1)
+ fizzBuzz(a: 100)
+ */
 
 
